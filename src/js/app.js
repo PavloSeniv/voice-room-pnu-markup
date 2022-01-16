40bae3cd610890e33a0d51d43ee25211306fc659
PavloSeniv <senivpavlo016@gmail.com>
2022-01-16 13:52:36 +0200

    Delete trash files and folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,2 @@
 import * as functionsIsWebp from "./modules/_webp_add_class_to_html.js";
 functionsIsWebp.isWebp();
-
-import * as functionsHeaderMenuBurger from "./modules/_header_menu_burger.js";
-functionsHeaderMenuBurger.headerMenuBurger();
-
-// $(function(){
-
-// });
