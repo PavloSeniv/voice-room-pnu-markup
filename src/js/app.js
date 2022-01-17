a0f751ff5de66a7ba0a5650ab7ef458fb249ae2e
PavloSeniv <senivpavlo016@gmail.com>
2022-01-17 17:11:02 +0200

    Add modal logic js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,6 @@
 import * as functionsIsWebp from "./modules/_webp_add_class_to_html.js";
 functionsIsWebp.isWebp();
+
+//Pop Up from modules
+import * as functionsPopUp from "./modules/plugins_pop_up.js";
+functionsPopUp.popUp();
---
+++ src/js/modules/plugins_pop_up.js
@@ -0,0 +1,41 @@
+export function popUp(params) {
+  let popupBg = document.querySelector(".pop-up__bg-container"); // Фон попап окна
+  let popup = document.querySelector(".pop-up"); // Само окно
+  let closePopupButton = document.querySelector(".pop-up__close"); //  Кнопка для скрытия окна
+  let openPopupButtonsAll = document.querySelectorAll(".rooms__new-room"); // Кнопки для показа окна
+  let openPopupButtons = document.querySelector(".rooms__new-room"); // Кнопки для показа окна
+  let openPopupBlock = document.querySelector(".rooms__container");
+  let bodyNoScroll = document.querySelector(".body");
+
+  openPopupButtonsAll.forEach((button) => {
+    // Перебираем все кнопки
+    button.addEventListener("click", (e) => {
+      // Для каждой вешаем обработчик событий на клик
+      e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+      popupBg.classList.add("active"); // Добавляем класс 'active' для фона
+      popup.classList.add("active"); // И для самого окна
+      button.classList.add("active"); // И для самого окна
+      openPopupBlock.classList.add("active"); // И для самого окна
+      bodyNoScroll.classList.add("active"); // 
+    });
+  });
+
+  closePopupButton.addEventListener("click", () => {
+    // Вешаем обработчик на крестик
+    popupBg.classList.remove("active"); // Убираем активный класс с фона
+    popup.classList.remove("active"); // И с окна
+    openPopupButtons.classList.remove("active");
+    openPopupBlock.classList.remove("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    // Вешаем обработчик на весь документ
+    if (e.target === popupBg) {
+      // Если цель клика - фот, то:
+      popupBg.classList.remove("active"); // Убираем активный класс с фона
+      popup.classList.remove("active"); // И с окна
+      openPopupButtons.classList.remove("active");
+      openPopupBlock.classList.remove("active");
+    }
+  });
+}
